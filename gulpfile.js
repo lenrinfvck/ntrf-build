72abd68b434f6b48fcd022a499ce20936848bd63
Void <lenrinfvck@hotmail.com>
2015-09-15 17:11:11 +0800

    create package.json , gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,170 @@
+//编译less, swig, 打包压缩
+
+"use strict";
+
+var gulp = require("gulp"),
+	path = require("path"),
+	fs,
+	msgErrs = {};
+
+(function() {
+	try {
+		fs = require("fs-extra");
+	} catch (ex) {
+		fs = require("fs");
+	}
+})();
+
+
+/**
+ * 寻找项目根目录
+ * @return {String} 项目根目录地址
+ */
+function findRoot() {
+	var dir = process.argv.indexOf("--path"),
+		i;
+	return dir >= 0 ? process.argv[dir + 1] : (function() {
+		var paths = [".", "../test"];
+		for (i = 0; i < paths.length; i++) {
+			dir = paths[i];
+			if (fs.existsSync(path.join(dir, "index.html"))) {
+				return dir;
+			}
+		}
+		return ".";
+	})();
+}
+
+
+/**
+ * 编译
+ */
+function complier(opt) {
+	var less = require("gulp-less"),
+		uglify = require("gulp-uglify"),
+		autoprefixer = require("gulp-autoprefixer"),
+		minifycss = require("gulp-minify-css"),
+		// imagemin = require("gulp-imagemin"),
+		rename = require("gulp-rename"),
+		// clean = require("gulp-clean"),
+		// concat = require("gulp-concat"),
+		notify = require("gulp-notify"),
+		replace = require("gulp-replace"),
+		plumber = require("gulp-plumber"),
+		filter = require("gulp-filter"),
+		gulpif = require("gulp-if"),
+		wraper = require("gulp-wrapper"),
+		// livereload = require("gulp-livereload");
+		browserSync = require("browser-sync"),
+		includer = require("gulp-file-include");
+
+	var css = opt.css,
+		js = opt.js,
+		html = opt.html;
+
+	var util = {
+		css: function(files) {
+			files
+				.pipe(plumber({ //错误处理
+					errorHandler: errrHandler
+				}))
+				.pipe(gulpif(css.filter && css.filter.length, filter(css.filter)))
+				.pipe(less())
+				.pipe(autoprefixer({
+					browsers: ["last 3 version", "ie > 6", "Android >= 3", "Safari >= 5.1", "iOS >= 5"]
+				}))
+				.pipe(gulpif(opt.debug, minifycss({
+					compatibility: "ie6"
+				})))
+				.pipe(sourcemaps.write(".", {
+					sourceRoot: "/" + path.relative(opt.rootPath, opt.styleSrc).replace(/\\/g, "/")
+				}))
+				.pipe(gulp.dest(css.dest))
+				.pipe(browserSync.stream()) //browser sync 自动刷新
+				.pipe(notify({
+					message: "CSS complied!"
+				}));
+		},
+		js: function(files) {
+			var modFilter = filter(["**/*.module.js"]), //module筛选，不编译
+				jsFilter = filter(["*.js", "!**/*.module.js"]);
+			files
+				.pipe(plumber({ //错误处理
+					errorHandler: errrHandler
+				}))
+				.pipe(replace(/\r\n?/g, "\n")) //统一换行符
+				.pipe(gulpif(!opt.debug, uglify())) //压缩
+				.pipe(jsFilter)
+				.pipe(gulp.dest(js.dest))
+				.pipe(jsFilter.restore())
+				.pipe(modFilter)
+				.pipe(rename(function(path) {
+					path.basename = path.basename.replace(/\.\w+$/, "");
+				}))
+				.pipe(wraper({
+					header: function(file) {
+						return "(function(f){typeof define===\"function\"?define(" + moduleName(file, js.src) + ",f):f()})(function(require,exports,module){";
+					},
+					footer: "});"
+				}))
+				.pipe(gulp.dest(js.dest))
+				.pipe(notify({
+					message: "JS complied！"
+				}));
+		},
+		html: function(files) {
+			files
+				.pipe(plumber({ //错误处理
+					errorHandler: errrHandler
+				}))
+				.pipe(gulpif(html.filter && html.filter.length, filter(html.filter)))
+				.pipe(includer())
+				.pipe(gulp.dest(html.dest)).pipe(notify({
+					message: "HTML complied！"
+				}));
+		}
+	};
+
+	/*CSS 处理*/
+
+	util.css(gulp.src(css.src + "**/*.less"));
+
+	/*JS 处理*/
+	util.js(gulp.src(js.src + "**/*.js"));
+
+	/*HTML 处理*/
+	util.html(gulp.src(html.src + "**/*.html"));
+
+	/*监控文件改变*/
+	//less
+	gulp
+		.watch(css.src + "**/*.less", function(files) {
+			console.log(files);
+			util.css(gulp.src(files.path));
+		});
+
+	//js
+	gulp
+		.watch(js.src + "**/*.js", function(files) {
+			util.js(gulp.src(files.path));
+		});
+
+	//html
+	gulp
+		.watch(html.src + "**/*.html", function(files) {
+			util.html(gulp.src(files.path));
+		});
+
+	setTimeout(function() {
+		console.log("-------browser sync---------");
+
+		browserSync.init({
+			server: {
+				baseDir: opt.rootPath
+			}
+		});
+
+		gulp.watch([html.dest + "**/*.html", js.dest + "**/*.js"], browserSync.reload);
+	}, 200);
+
+}
